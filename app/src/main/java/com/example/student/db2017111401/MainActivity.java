139b3a1a500fa4e41792849e2978e55f73b5736c
Shaojung <shaojung@gmail.com>
2017-11-14 20:46:52 +0800

    Add Buttons

--- app/src/main/java/com/example/student/db2017111401/MainActivity.java
+++ app/src/main/java/com/example/student/db2017111401/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.student.db2017111401;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Test1");
+        menu.add("Test2");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
